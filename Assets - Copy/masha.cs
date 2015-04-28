c4ca422abf2a4849c7f56ab980b3ee87f438edc8
illah <ilahdilah3@gmail.com>
2015-04-28 19:06:22 +0700

    fudail

---
+++ Assets - Copy/masha.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class masha : MonoBehaviour {
+	public float jump;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Rigidbody>().velocity = Vector3.zero;
+			GetComponent<Rigidbody>().AddForce(Vector3.up * jump);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			transform.position +=(Vector3.left/10);		
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			transform.position +=(Vector3.right/10);		
+		}
+	}
+}
---
+++ Assets/serigala.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class serigala : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		transform.position +=(Vector3.right/30);
+	}
+}
